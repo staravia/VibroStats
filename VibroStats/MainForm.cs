ecb8244e37b85221b78a5b5f9928b3e739b1b6ec
staravia <denysenesouma@gmail.com>
2017-10-31 18:31:46 -0700

    Update: Calculations

--- VibroStats/MainForm.cs
+++ VibroStats/MainForm.cs
@@ -28,19 +28,19 @@ namespace vibromark
         private bool TenSecondMode { get; set; }
 
         /// <summary>
-        /// Determines which keys are pressed down.
+        /// Determines which KeysDown are pressed down.
         /// </summary>
-        private bool[] keys { get; set; }
+        private bool[] KeysDown { get; set; }
 
         /// <summary>
-        /// 
+        /// Used for simple NPS calculation
         /// </summary>
-        private int[] curData { get; set; }
+        private int[] ShortData { get; set; }
 
         /// <summary>
-        /// 
+        /// Used to calculate NPS/consistancy of each finger
         /// </summary>
-        private int[,] allData { get; set; }
+        private int[,] LongData { get; set; }
 
         /// <summary>
         /// 
@@ -57,6 +57,8 @@ namespace vibromark
         /// </summary>
         private bool IsRunning { get; set; }
 
+        private const int SleepTime = 100;
+
         delegate void SetTextCallback(string cooler, string bpma, int progressa);
 
         /// <summary>
@@ -65,9 +67,9 @@ namespace vibromark
         public MainForm()
         {
             IsRunning = true;
-            keys = new bool[4];
-            curData = new int[4];
-            allData = new int[4, 0];
+            KeysDown = new bool[4];
+            ShortData = new int[4];
+            LongData = new int[4, 0];
             PrevAverageBPM = new float[100];
             InitializeComponent();
         }
@@ -102,7 +104,7 @@ namespace vibromark
             if (!IsAnalyzingTest)
             {
                 IsAnalyzingTest = true;
-                label2.Text = "Vibrate on assigned keys to begin";
+                label2.Text = "Vibrate on assigned KeysDown to begin";
                 label3.Text = "00.0 bpm";
                 progressBar1.Value = 0;
                 groupBox1.Hide();
@@ -110,11 +112,11 @@ namespace vibromark
                 PrevAverageBPM = new float[100];
                 if (TenSecondMode)
                 {
-                    allData = new int[4, 500];
+                    LongData = new int[4, (int)(10000/SleepTime)];
                 }
                 else
                 {
-                    allData = new int[4, 1250];
+                    LongData = new int[4, (int)(25000/ SleepTime)];
                 }
                 Task measureVibro = Task.Factory.StartNew(() => TrackTap())
                     .ContinueWith((t1) => doneTrack(), TaskScheduler.FromCurrentSynchronizationContext());
@@ -147,13 +149,12 @@ namespace vibromark
 
         private void doneTrack()
         {
+            //Update Main Window
             groupBox3.Hide();
             groupBox1.Show();
 
-
-            //CALCULATIONS
-
-            int curLength = (int)allData.Length / 4;
+            //Assign Calculation Variables
+            int curLength = (int)LongData.Length / 4;
             float[,] averagebpm = new float[4, curLength];
             float[] averagebpm0 = new float[curLength];
             float[] averagebpm1 = new float[curLength];
@@ -179,12 +180,12 @@ namespace vibromark
                     {
                         if (a<=50 && j+a<curLength)
                         {
-                            tempStats[i, j] = tempStats[i, j] + allData[i,j+a];
+                            tempStats[i, j] = tempStats[i, j] + LongData[i,j+a];
                             counter++;
                         }
                         else if (a > 50 && j+50-a >= 0)
                         {
-                            tempStats[i, j] = tempStats[i, j] + allData[i, j+50 - a];
+                            tempStats[i, j] = tempStats[i, j] + LongData[i, j+50 - a];
                             counter++;
                         }
                     }
@@ -246,13 +247,13 @@ namespace vibromark
 
                 }
             }
+
+            //If the player has pressed any keys at all
             if (averagebpm0.Average() + averagebpm1.Average() + averagebpm2.Average() + averagebpm3.Average() > 0)
             {
-
                 //FIND PEAK/STAMINA
                 for (int j = 0; j < curLength; j++)
                 {
-                    counter = 0;
                     if (j > 25 && (averagebpm0[j] + averagebpm1[j]) / 2f > bestBpml[1])
                     {
                         bestBpml[1] = (averagebpm0[j] + averagebpm1[j]) / 2f;
@@ -290,48 +291,48 @@ namespace vibromark
 
                 }
 
-
-                //CREATE WINDOW
+                //Create result screen
                 StatsWindow frm = new StatsWindow();
-                //ADD POINTS TO GRAPH
+
+                //Plot graph if player has vibroed
                 if (bestBpml[1]>160 || bestBpmr[1] > 160)
                 {
-                    //IS VIBRO
                     for (int i = 0; i < curLength; i++)
                     {
-
+                        //Plots a point every 10 data points
                         if (i % 10 == 0)
                         {
-                            frm.bpmChart.Series["rhBpm"].Points.AddXY
-                                ((float)i * 20 / 1000f, (averagebpm[2, i] + averagebpm[3, i]) / 2f);
-                            frm.bpmChart.Series["lhBpm"].Points.AddXY
-                                ((float)i * 20 / 1000f, (averagebpm[0, i] + averagebpm[1, i]) / 2f);
+                            frm.bpmChart.Series["rhBpm"].Points.AddXY((float)i * 20 / 1000f, (averagebpm[2, i] + averagebpm[3, i]) / 2f);
+                            frm.bpmChart.Series["lhBpm"].Points.AddXY((float)i * 20 / 1000f, (averagebpm[0, i] + averagebpm[1, i]) / 2f);
                         }
                     }
                 }
+
+                //If no points are on graph, create nil graph.
                 else
                 {
                     frm.bpmChart.Series["rhBpm"].Points.AddXY(0, 161);
                     frm.bpmChart.Series["lhBpm"].Points.AddXY(0, 161);
                 }
 
+                //Calculate Average BPM
                 double curAvgBpm = Math.Round((averagebpm0.Average() + averagebpm1.Average() + averagebpm2.Average() + averagebpm3.Average()) / 4f, 2);
-                frm.averageBPM.Text = curAvgBpm.ToString();
-                frm.dateText.Text = DateTime.Now.ToString();
-                frm.L_medianBPM.Text = "Median BPM: " + Math.Round((GetMedian(averagebpm0) + GetMedian(averagebpm1)) / 2, 2).ToString() + "bpm";
-                frm.R_medianBPM.Text = "Median BPM: " + Math.Round((GetMedian(averagebpm2) + GetMedian(averagebpm3)) / 2, 2).ToString() + "bpm";
-                frm.L_peakBPM.Text = "Peak BPM: " + Math.Round(bestBpml[1], 2).ToString() + "bpm";
-                frm.R_peakBPM.Text = "Peak BPM: " + Math.Round(bestBpmr[1], 2).ToString() + "bpm";
-                frm.L_BpmDrain.Text = "Stamina Rate: " + Math.Round(Math.Min(0,(60f * (averageUR0.Average() + averageUR1.Average()))) / 2f, 2).ToString() + "bpm²";
-                frm.R_bpmDrain.Text = "Stamina Rate: " + Math.Round(Math.Min(0,(60f * (averageUR2.Average() + averageUR3.Average()))) / 2f, 2).ToString() + "bpm²";
 
+                //Calculate Peak/Median BPM
+                double L_medianBPM = (GetMedian(averagebpm0) + GetMedian(averagebpm1)) / 2;
+                double L_peakkBPM = bestBpml[1];
+                double R_medianBPM = (GetMedian(averagebpm2) + GetMedian(averagebpm3)) / 2;
+                double R_peakBPM = bestBpmr[1];
+
+                //Calculate BPM Drain
+                double L_BpmDrain = Math.Min(0, (60f * (averageUR0.Average() + averageUR1.Average()))) / 2f;
+                double R_BpmDrain = Math.Min(0, (60f * (averageUR2.Average() + averageUR3.Average()))) / 2f;
+
+                //Calculate BPM of Stamina Drain
                 double staminaBpmL = (GetMedian(averagebpm0) + GetMedian(averagebpm1)) / 2f - Math.Pow((averageUR0.Average() + averageUR1.Average()) * 60f / 2f, 2f);
                 double staminaBpmR = (GetMedian(averagebpm2) + GetMedian(averagebpm3)) / 2f - Math.Pow((averageUR2.Average() + averageUR3.Average()) * 60f / 2f, 2f);
 
-                frm.L_staminaDrain.Text = "Stamina BPM: " + Math.Round(staminaBpmL, 2).ToString() + "bpm";
-                frm.R_staminaDrain.Text = "Stamina BPM: " + Math.Round(staminaBpmR, 2).ToString() + "bpm";
-
-                //CONVERT TO MAGNITUDE
+                //Convert UR to magnitude
                 for (int i = 0; i < curLength - 1; i++)
                 {
                     averageUR0[i] = Math.Abs(averageUR0[i]);
@@ -339,6 +340,8 @@ namespace vibromark
                     averageUR2[i] = Math.Abs(averageUR2[i]);
                     averageUR3[i] = Math.Abs(averageUR3[i]);
                 }
+
+                //Calculate UR
                 double bpmURl = (10000d / 6d) * (averageUR0.Select(val => (val - Math.Pow((averageUR0.Average() + averageUR1.Average()) / 2,2))).Sum()
                     + averageUR1.Select(val => (val - Math.Pow((averageUR0.Average() + averageUR1.Average()) / 2, 2))).Sum()) 
                     / curLength;
@@ -346,63 +349,74 @@ namespace vibromark
                     + averageUR3.Select(val => (val - Math.Pow((averageUR2.Average() + averageUR3.Average()) / 2, 2))).Sum())
                     / curLength;
 
-                //genURl = Math.Pow((100f / 6f) * (averageUR0. - averageUR1.Average()), 2) / (curAvgBpm / 15000);
+                //Calculate General Unstable Rate
                 double genURl = Math.Pow((100d / 6d) * (averageUR0.Average() - averageUR1.Average()), 2)/(curAvgBpm / 15000);
                 double genURr = Math.Pow((100d / 6d) * (averageUR2.Average() - averageUR3.Average()), 2) / (curAvgBpm / 15000);
-                //double bpmURl = Math.Pow((100f / 6f) * (Math.Abs(averageUR0.Average()) + Math.Abs(averageUR1.Average())) / 2f, 2) / (curAvgBpm / 15000); OLD
-                //double bpmURr = Math.Pow((100f / 6f) * (Math.Abs(averageUR2.Average()) + Math.Abs(averageUR3.Average())) / 2f, 2) / (curAvgBpm / 15000); OLD
+
+                //Set Text of top body
+                frm.averageBPM.Text = curAvgBpm.ToString();
+                frm.dateText.Text = DateTime.Now.ToString();
+
+                //Set Text of bottom body (Unstability)
                 frm.L_GeneralUnstability.Text = "General Instability: " + Math.Round(genURl,4);
                 frm.R_Unstability.Text = "General Instability: " + Math.Round(genURr,4);
                 frm.L_BpmUnstability.Text = "BPM Unstable Rate: " + Math.Round(bpmURl,1);
                 frm.R_BpmUnstability.Text = "BPM Unstable Rate: " + Math.Round(bpmURr,1);
 
+                //Set Text of bottom body (Average/Median BPM)
+                frm.L_medianBPM.Text = "Median BPM: " + Math.Round(L_medianBPM, 2) + "bpm";
+                frm.R_medianBPM.Text = "Median BPM: " + Math.Round(R_medianBPM, 2) + "bpm";
+                frm.L_peakBPM.Text = "Peak BPM: " + Math.Round(L_peakkBPM, 2) + "bpm";
+                frm.R_peakBPM.Text = "Peak BPM: " + Math.Round(R_peakBPM, 2) + "bpm";
 
+                //Set Text of bottom body (Stamina Drain)
+                frm.L_BpmDrain.Text = "Stamina Rate: " + Math.Round(L_BpmDrain, 2) + "bpm²";
+                frm.R_bpmDrain.Text = "Stamina Rate: " + Math.Round(R_BpmDrain, 2) + "bpm²";
+                frm.L_staminaDrain.Text = "Stamina BPM: " + Math.Round(staminaBpmL, 2) + "bpm";
+                frm.R_staminaDrain.Text = "Stamina BPM: " + Math.Round(staminaBpmR, 2) + "bpm";
+
+                //Calculate HandPref
                 string handtextr = "";
-                double handPref;
-                handPref = Math.Round(Math.Min(100 * ((averagebpm0.Average() + averagebpm1.Average()) / (averagebpm2.Average() + averagebpm3.Average()) - 1f), 100),1);
-                if (handPref == 0)
-                {
-                    handtextr = "Equal Hand Preference";
-                }
-                else if (handPref > 0)
-                {
-                    handtextr = handPref + "% Left Hand Preference";
-                }
-                else
-                {
-                    handtextr = Math.Abs(handPref) + "% Right Hand Preference";
-                }
+                double handPref = Math.Round(Math.Min(100 * ((averagebpm0.Average() + averagebpm1.Average()) / (averagebpm2.Average() + averagebpm3.Average()) - 1f), 100),1);
+
+                //Set Hand Pref Text
+                if (handPref == 0)  handtextr = "Equal Hand Preference";
+                else if (handPref > 0) handtextr = handPref + "% Left Hand Preference";
+                else handtextr = Math.Abs(handPref) + "% Right Hand Preference";
                 frm.handPrefText.Text = handtextr;
-                frm.averageUR.Text = Math.Round(((genURl+ genURr)/2f + (bpmURl+ bpmURr)/2f),2).ToString();
-                
-                double curAcc = 100/Math.Log10(10f + ((genURl + genURr) / 2f + (bpmURl + bpmURr) / 2f)/60f);
-                double curScore = Math.Pow(curAcc/100,0.8)*Math.Pow((staminaBpmL+staminaBpmR)/(Math.Max(2 * curAvgBpm, 1)), 1.4)*Math.Pow(curAvgBpm/175,2.2)*2000;
 
+                //Calculate Main Vibro Stats
+                double averageUR = ((genURl + genURr) / 2f + (bpmURl + bpmURr) / 2f);
+                double curAcc = 100 / Math.Log10(10f + ((genURl + genURr) / 2f + (bpmURl + bpmURr) / 2f) / 60f);
+                double curScore = Math.Pow(curAcc / 100, 0.8) * Math.Pow((staminaBpmL + staminaBpmR) / (Math.Max(2 * curAvgBpm, 1)), 1.4) * Math.Pow(curAvgBpm / 175, 2.2) * 2000;
 
+                //Set Main Vibro Stat Text
+                frm.averageUR.Text = Math.Round(averageUR,2).ToString();
                 frm.accText.Text = Math.Round(curAcc,2)+"%";
                 frm.scoreText.Text = Math.Floor(curScore).ToString();
 
+                //Show Results
                 frm.Show();
-
             }
+            //Finish Test
             IsAnalyzingTest = false;
         }
 
         private void TrackTap()
         {
             int interval = 0;
-            curData = new int[4];
+            ShortData = new int[4];
 
             //Wait until a key is pressed
-            while (curData[0]== 0 && curData[1] == 0 && curData[2] == 0 && curData[3] == 0 && interval<200)
+            while (ShortData[0]== 0 && ShortData[1] == 0 && ShortData[2] == 0 && ShortData[3] == 0 && interval<40)
             {
-                Thread.Sleep(20);
+                Thread.Sleep(100);
                 interval++;
             }
 
             //Start analyzing data if player has tapped within 4 seconds
-            curData = new int[4];
-            if (interval < 200)
+            ShortData = new int[4];
+            if (interval < 40)
             {
                 //Set how long the analyze time is in miliseconds
                 int waittime = 10000;
@@ -412,20 +426,20 @@ namespace vibromark
                 interval = 0;
 
                 //Loop through until analyzing is done
-                while (interval * 20 < waittime)
+                while (interval * SleepTime < waittime)
                 {
-                    var tempb = (int)Math.Ceiling((waittime - interval * 20) / 1000f);
-                    Thread.Sleep(20);
+                    var tempb = (int)Math.Ceiling((waittime - interval * SleepTime) / 1000f);
+                    Thread.Sleep(SleepTime);
                     CurrentAverageBPM = 0;
                     for (int i = 0; i< 4; i++)
                     {
-                        allData[i, interval] = curData[i];
-                        curData[i] = 0;
+                        LongData[i, interval] = ShortData[i];
+                        ShortData[i] = 0;
                         if (interval >= 10)
                         {
                             for (int j = 0; j < 10; j++)
                             {
-                                CurrentAverageBPM += allData[i, interval - j];
+                                CurrentAverageBPM += LongData[i, interval - j];
                             }
                         }
                     }
@@ -434,8 +448,8 @@ namespace vibromark
                         PrevAverageBPM[i] = PrevAverageBPM[i+1];
                     }
                     PrevAverageBPM[99] = CurrentAverageBPM;
-                    CurrentAverageBPM = PrevAverageBPM.Average() * 15 * 5 / 4;
-                    this.hideTip(tempb.ToString() + " seconds remaining", Math.Round(CurrentAverageBPM,1)+"bpm",(int)Math.Ceiling((double)(interval * 100* 20/waittime)));
+                    CurrentAverageBPM = PrevAverageBPM.Average() * 15 * SleepTime / 1000;
+                    hideTip(tempb + " seconds remaining", Math.Round(CurrentAverageBPM,1)+"bpm",(int)Math.Ceiling((double)(interval * 100* SleepTime/ waittime)));
                     CurrentAverageBPM = CurrentAverageBPM * 75f / 4f;
                     interval += 1;
                 }
@@ -533,34 +547,34 @@ namespace vibromark
                     }
                     if ((textBox1.Text != "") && (az.Equals(textBox1.Text[0].ToString().ToUpper())))
                     {
-                        if (!keys[0])
+                        if (!KeysDown[0])
                         {
-                            keys[0] = true;
-                            curData[0]++;
+                            KeysDown[0] = true;
+                            ShortData[0]++;
                         }
                     }
                     else if ((textBox2.Text != "") && (az.Equals(textBox2.Text[0].ToString().ToUpper())))
                     {
-                        if (!keys[1])
+                        if (!KeysDown[1])
                         {
-                            keys[1] = true;
-                            curData[1]++;
+                            KeysDown[1] = true;
+                            ShortData[1]++;
                         }
                     }
                     else if ((textBox3.Text != "") && (az.Equals(textBox3.Text[0].ToString().ToUpper())))
                     {
-                        if (!keys[2])
+                        if (!KeysDown[2])
                         {
-                            keys[2] = true;
-                            curData[2]++;
+                            KeysDown[2] = true;
+                            ShortData[2]++;
                         }
                     }
                     else if ((textBox4.Text != "") && (az.Equals(textBox4.Text[0].ToString().ToUpper())))
                     {
-                        if (!keys[3])
+                        if (!KeysDown[3])
                         {
-                            keys[3] = true;
-                            curData[3]++;
+                            KeysDown[3] = true;
+                            ShortData[3]++;
                         }
                     }
                 }
@@ -585,19 +599,19 @@ namespace vibromark
                     }
                     if ((textBox1.Text != "") && (az.Equals(textBox1.Text[0].ToString().ToUpper())))
                     {
-                        keys[0] = false;
+                        KeysDown[0] = false;
                     }
                     else if ((textBox2.Text != "") && (az.Equals(textBox2.Text[0].ToString().ToUpper())))
                     {
-                        keys[1] = false;
+                        KeysDown[1] = false;
                     }
                     else if ((textBox3.Text != "") && (az.Equals(textBox3.Text[0].ToString().ToUpper())))
                     {
-                        keys[2] = false;
+                        KeysDown[2] = false;
                     }
                     else if ((textBox4.Text != "") && (az.Equals(textBox4.Text[0].ToString().ToUpper())))
                     {
-                        keys[3] = false;
+                        KeysDown[3] = false;
                     }
                 }
             }
